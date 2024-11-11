36323f7dcd01e33fdb125dd4bb98917037e8c793
Jjastiny <jjastiny@gmail.com>
2024-11-10 22:42:20 -0500

    feat: change buidl gradle for network and data list to support retrofit

--- data/data-list/build.gradle.kts
+++ data/data-list/build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.fetch.android.library)
+    alias(libs.plugins.fetch.hilt)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_18
-    targetCompatibility = JavaVersion.VERSION_18
+android {
+    namespace = "com.fetch.data.list"
+}
+
+dependencies {
+    implementation(project(":core:common"))
+    implementation(project(":core:network"))
 }
