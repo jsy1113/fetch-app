6be51da9f6b2541696262f2536d007c13804e476
Jjastiny <jjastiny@gmail.com>
2024-11-11 00:04:49 -0500

    feat: add missing repository module

---
+++ data/data-list/src/main/kotlin/com/fetch/data/list/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.fetch.data.list.di
+
+import com.fetch.data.list.repository.ItemRepository
+import com.fetch.data.list.repository.ItemRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    internal abstract fun bindsEmployeeRepository(itemRepositoryImpl: ItemRepositoryImpl): ItemRepository
+
+}
